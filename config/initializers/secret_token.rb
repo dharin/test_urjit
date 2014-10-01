64da60550cf42ed0f38bc43a4968ae5ff32a30f6
dharin <dharin.rajgor@gmail.com>
2014-10-01 12:54:17 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestUrjit::Application.config.secret_key_base = 'e9d826de91d4f1c098d3c6770995fd7143ea9ad16bbbdab91fc550bbd9af2bada7bb45c3491d0cddb065d885d40e7580be18534862437774fe2e77322dd68429'
